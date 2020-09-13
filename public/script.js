af4fc18f7030822a94a69c31672c5f07757354c8
Anurag Keerthi <anuragkeerthi@gmail.com>
2020-09-13 16:29:49 +0530

    Added Play and Stop Video Feature

--- public/script.js
+++ public/script.js
@@ -12,7 +12,7 @@ var peer = new Peer(undefined, {
 let myVideoStream
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: false
+    audio: true
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
@@ -70,3 +70,59 @@ const scrollToBottom = () => {
     let d = $('.main_chat_window');
     d.scrollTop(d.prop("scrollHeight"));
 }
+
+const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if(enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    } else {
+        setMuteButton();
+        myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+}
+
+const setMuteButton = () => {
+    const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+    const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+
+const playStop = () => {
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if(enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo()
+    } else {
+        setStopVideo()
+        myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+const setPlayVideo = () => {
+    const html = `
+    <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setStopVideo = () => {
+    const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+}
+
+
